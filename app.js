56a041223fb730c58b7690b01f2dc66d93b395c1
Vishwajeetsinh Rathod <rathodvishwajeet64@gmail.com>
2023-05-17 10:45:32 +0530

    changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ primaryHeader.before(scrollWatcher)
 const navObserver = new IntersectionObserver((en) => {
     console.log(en)
     primaryHeader.classList.toggle('sticking', !    en[0].isIntersecting)
+    // root margin is used for applying style when window scrolled 200px 
 }, {rootMargin: '200px 0px 0px 0px'})
 
 navObserver.observe(scrollWatcher)
